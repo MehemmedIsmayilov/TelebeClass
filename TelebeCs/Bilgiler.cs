eae5a269f4b3204295d0bdd6cda858f7632b5024
MehemmedIsmayilov <mehemmedmi@code.edu.az>
2022-11-29 01:59:00 +0400

    Telebe Bilgileri

---
+++ TelebeCs/Bilgiler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelebeCs
+{
+    public class TelebeBilgi
+    {
+        public string Name;
+        public string Surname;
+        public byte GroupPoint;
+        public bool Graduated = true;
+
+        public TelebeBilgi(string name, string surname, byte groupPoint, bool graduated)
+        {
+            Name = name;
+            Surname = surname;
+            GroupPoint = groupPoint;
+            Graduated = graduated;
+        }
+
+
+    }
+}
---
+++ TelebeCs/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace TelebeCs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine();
+
+
+            //  TelebeBilgi telebeBilgi = new TelebeBilgi(Console.ReadLine(),Console.ReadLine(),Convert.ToByte(Console.ReadLine()),false);   //True variant
+            //  TelebeBilgi telebeBilgi1 = new TelebeBilgi(Console.ReadLine(),Console.ReadLine(),Convert.ToByte(Console.ReadLine()),true);  // False variant
+
+            TelebeBilgi telebeBilgi3 = new TelebeBilgi("Mehemmed", "Ismayilov", 0, true);
+            TelebeBilgi telebeBilgi4 = new TelebeBilgi("Hemid", "Cemilli", 90, false);
+
+            GeriQaytar(telebeBilgi3.Name,telebeBilgi3.Surname,telebeBilgi3.GroupPoint,telebeBilgi3.Graduated);
+            GeriQaytar(telebeBilgi4.Name,telebeBilgi4.Surname,telebeBilgi4.GroupPoint,telebeBilgi4.Graduated);
+
+            
+        }
+
+
+        static int GeriQaytar (string nameM,string surnameM,byte groupPointM,bool graduatedM)
+        {
+            
+
+           if (graduatedM==false)
+
+            {
+                if(groupPointM>=80)
+                {
+                    Console.WriteLine("Novbeti imtahana giris haqqi qazandiniz");
+                }
+                else
+                {
+                    Console.WriteLine("Novbeti imtahana giris haqqi qazanmadiniz");
+                }
+            }
+
+           else
+            {
+                Console.WriteLine("Telebemiz Artiq Mezundur");
+            }
+
+
+           
+
+
+          
+            return 0;
+        }
+        
+    }
+}
